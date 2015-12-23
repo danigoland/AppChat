8bd6ec0337e06db3f99308345bbcb49b609241cf
DekelYossef <dkl.yossef@gmail.com>
2015-12-23 21:27:54 +0200

    -- Initial commit --

---
+++ db.js
@@ -0,0 +1,68 @@
+var mongoose = require("mongoose");
+var userMessages;
+
+function dbConnect() {
+    mongoose.connect('mongodb://localhost/AppChatDB');
+}
+
+function messagesInit() {
+    if (!userMessages)
+        userMessages = mongoose.model('messages', {from: String, to: String, body: String, time: Date});
+    return userMessages;
+}
+
+function getMessageModel() {
+    if (!userMessages)
+        return messagesInit();
+    else
+        return userMessages
+}
+
+function getMessagesByQuery(query, callback) {
+    if (userMessages)
+        userMessages.find(query, function (err, data) {
+            return callback(err, data);
+        });
+    else
+        return callback("the Messages  model didn't initiated");
+}
+
+function getMessagesAll(callback) {
+    if (userMessages)
+        userMessages.find(function (err, data) {
+            return callback(err, data);
+        });
+    else
+        return callback("the Messages  model didn't initiated");
+}
+
+function getMessagesHistoryByName(name, callback) {
+    if (userMessages)
+        userMessages.find({$or: [{from: name}, {to: name}]}, null,
+            {sort: {date: -1}}, function (err, data) {
+                return callback(err, data);
+            });
+    else
+        return callback("the Messages  model didn't initiated");
+}
+
+function saveMessage(msg, callback) {
+    msg.save(function (err) {
+        if (err) // ...
+            callback(err);
+        else
+            callback();
+    });
+}
+
+module.exports = {
+    connect: dbConnect,
+    messages: {
+        init: messagesInit,
+        getAll: getMessagesAll,
+        getByQuery: getMessagesByQuery,
+        getHistoryByName: getMessagesHistoryByName,
+        save: saveMessage,
+        model: getMessageModel
+    }
+};
